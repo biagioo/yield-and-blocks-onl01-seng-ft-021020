e72b1b011a4ec4cf549cd293a6a663c2a888fe48
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-12 18:55:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -19,11 +19,11 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with? ("T")
-    puts "Hi, #{name}"
-  end 
-end
+# ["Tim", "Tom", "Jim"].each do |name|
+#   if name.start_with? ("T")
+#     puts "Hi, #{name}"
+#   end 
+# end
 
 def hello_t(array)
   i = 0 
